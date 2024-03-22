6b32281684b60cec1cc4c1fc0c9c0d63a055d81d
sam <mail@samueljenks.me>
2024-03-22 14:54:35 +1300

    fix: not found page

--- client/src/pages/notfound/notfound.view.tsx
+++ client/src/pages/notfound/notfound.view.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import '../../App.css'
-import './notfound.css'
-import { Link } from 'react-router-dom'
-import { homepath } from '../home/home.presenter'
+import React from "react";
+import "../../App.css";
+import "./notfound.css";
+import { Link } from "react-router-dom";
+import { homepath } from "../home/home.presenter";
 
 export const Notfound = (): JSX.Element => (
-    <div className="font centered-text">
-        Page does not exist. Click to go back
-        <Link className="button" to={homepath}>Home.</Link>
+  <div className="container">
+    <div className="container-item font text">
+      Page does not exist. Click to go back
     </div>
-)
+    <div className="container-item">
+      <Link className="button font" to={homepath}>
+        Home.
+      </Link>
+    </div>
+  </div>
+);
