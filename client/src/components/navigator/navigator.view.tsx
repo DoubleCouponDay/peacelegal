ec1b59320ca8b702b309c028fbfa6db95f467b3c
sam <mail@samueljenks.me>
2024-03-22 13:47:41 +1300

    refactor: new name

--- client/src/components/navigator/navigator.view.tsx
+++ client/src/components/navigator/navigator.view.tsx
@@ -1,83 +1,97 @@
-import React, { CSSProperties } from "react"
-import { Link } from "react-router-dom"
-import { homepath } from "../../pages/home/home.presenter"
-import { servicespath } from "../../pages/services/services.presenter"
-import { providerpath } from "../../pages/fees/fees.presenter"
-import { contactpath } from "../../pages/contact/contact.presenter"
-import '../../App.css'
-import './navigator.css'
-import {buttonstate} from "../../models"
-import talksymbol from '../../resources/smashicons/006-home-message.svg'
-import mailsymbol from '../../resources/smashicons/026-email-5.svg'
-import passportsymbol from '../../resources/smashicons/040-profile.svg'
+import React, { CSSProperties } from "react";
+import { Link } from "react-router-dom";
+import { homepath } from "../../pages/home/home.presenter";
+import { servicespath } from "../../pages/services/services.presenter";
+import { providerpath } from "../../pages/fees/fees.presenter";
+import { contactpath } from "../../pages/contact/contact.presenter";
+import "../../App.css";
+import "./navigator.css";
+import { buttonstate } from "../../models";
+import talksymbol from "../../resources/smashicons/006-home-message.svg";
+import mailsymbol from "../../resources/smashicons/026-email-5.svg";
+import passportsymbol from "../../resources/smashicons/040-profile.svg";
 
 export function navigatorview(
-  mousedown: (buttonindex: number) => void, 
+  mousedown: (buttonindex: number) => void,
   mouseup: () => void,
-  homebutton: buttonstate, 
-  servicebutton: buttonstate, 
+  homebutton: buttonstate,
+  servicebutton: buttonstate,
   lspButton: buttonstate,
-  contactbutton: buttonstate): JSX.Element {
-    return (
-      <nav>        
-        <div className="divider-container">
-          <Link to={homepath} 
-            className="nav-item-text nav-title"   
-            draggable="false"         
-            onMouseDown={() => mousedown(0)}
-            onMouseUp={() => mouseup()}>
-              Mountain Law
-          </Link>
-          <div className="divider-line" />
-        </div>
+  contactbutton: buttonstate
+): JSX.Element {
+  return (
+    <nav>
+      <div className="divider-container">
+        <Link
+          to={homepath}
+          className="nav-item-text nav-title"
+          draggable="false"
+          onMouseDown={() => mousedown(0)}
+          onMouseUp={() => mouseup()}
+        >
+          Peace Legal
+        </Link>
+        <div className="divider-line" />
+      </div>
 
-        <div className="buttons-container">
-          <Link to={servicespath}
-            style={getbuttonsstyle(servicebutton, servicespath)}
-            className="nav-item"
-            draggable="false"      
-            onMouseDown={() => mousedown(1)}
-            onMouseUp={() => mouseup()}>
-              <img className="icon"
-              src={talksymbol} /> Services</Link>
+      <div className="buttons-container">
+        <Link
+          to={servicespath}
+          style={getbuttonsstyle(servicebutton, servicespath)}
+          className="nav-item"
+          draggable="false"
+          onMouseDown={() => mousedown(1)}
+          onMouseUp={() => mouseup()}
+        >
+          <img className="icon" src={talksymbol} /> Services
+        </Link>
 
-          <Link to={providerpath}
-            style={getbuttonsstyle(lspButton, providerpath)}
-            className="nav-item"  
-            draggable="false"                
-            onMouseDown={() => mousedown(2)}
-            onMouseUp={() => mouseup()}>
-              <img className="icon"
-              src={passportsymbol} />Fees</Link>
+        <Link
+          to={providerpath}
+          style={getbuttonsstyle(lspButton, providerpath)}
+          className="nav-item"
+          draggable="false"
+          onMouseDown={() => mousedown(2)}
+          onMouseUp={() => mouseup()}
+        >
+          <img className="icon" src={passportsymbol} />
+          Fees
+        </Link>
 
-          <Link to={contactpath}
-            style={getbuttonsstyle(contactbutton, contactpath)}
-            className="nav-item"
-            draggable="false"      
-            onMouseDown={() => mousedown(3)}
-            onMouseUp={() => mouseup()}>
-              <img className="icon"
-              src={mailsymbol} />Contact</Link>
-        </div>
-      </nav>
-    )
+        <Link
+          to={contactpath}
+          style={getbuttonsstyle(contactbutton, contactpath)}
+          className="nav-item"
+          draggable="false"
+          onMouseDown={() => mousedown(3)}
+          onMouseUp={() => mouseup()}
+        >
+          <img className="icon" src={mailsymbol} />
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export const buttoncount = 4
-export const homebuttonindex = 0
+export const buttoncount = 4;
+export const homebuttonindex = 0;
 
-const getbuttonsstyle = (state: buttonstate, buttonsroute: string): CSSProperties => {
-  let output: CSSProperties = {}
+const getbuttonsstyle = (
+  state: buttonstate,
+  buttonsroute: string
+): CSSProperties => {
+  let output: CSSProperties = {};
 
-  if(state.supressed === true ||
-    window.location.pathname === `${buttonsroute}`) {
-    output.boxShadow = suppressedbutton
+  if (
+    state.supressed === true ||
+    window.location.pathname === `${buttonsroute}`
+  ) {
+    output.boxShadow = suppressedbutton;
+  } else {
+    output.boxShadow = "";
   }
+  return output;
+};
 
-  else {
-    output.boxShadow = ""
-  }
-  return output
-}
-
-const suppressedbutton = "2px 2px 0px rgb(75, 202, 204)" 
+const suppressedbutton = "2px 2px 0px rgb(75, 202, 204)";
--- client/src/pages/home/home.view.tsx
+++ client/src/pages/home/home.view.tsx
@@ -1,20 +1,22 @@
-import React from "react"
-import "../../App.css"
-import {templaterow} from "../../models"
-import profilepic from "../../resources/nosatprofile.png"
-import wellies from '../../resources/wellington.png'
-import pagetemplate from "../../templates/page/page.template"
+import React from "react";
+import "../../App.css";
+import { templaterow } from "../../models";
+import profilepic from "../../resources/nosatprofile.png";
+import wellies from "../../resources/wellington.png";
+import pagetemplate from "../../templates/page/page.template";
 
 export default function Homeview(): JSX.Element {
-    let list: templaterow[] = [
-        {
-            imagepath: profilepic,
-            paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer urna ante, semper ac enim et, ultrices aliquet libero. Nunc id magna elit. Ut varius malesuada urna, commodo consequat ligula posuere viverra. Aliquam lobortis tellus nec ligula eleifend condimentum. Nullam volutpat nisi et sagittis consectetur. Integer fermentum a lorem quis aliquet. In laoreet sed leo et laoreet. Proin lacinia, lectus nec sagittis"
-        },
-        {
-            imagepath: wellies,
-            paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer urna ante, semper ac enim et, ultrices aliquet libero. Nunc id magna elit. Ut varius malesuada urna, commodo consequat ligula posuere viverra. Aliquam lobortis tellus nec ligula eleifend condimentum. Nullam volutpat nisi et sagittis consectetur. Integer fermentum a lorem quis aliquet. In laoreet sed leo et laoreet. Proin lacinia, lectus nec sagittis"
-        }
-    ]
-    return (pagetemplate("Mountain Law", list))
+  let list: templaterow[] = [
+    {
+      imagepath: profilepic,
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer urna ante, semper ac enim et, ultrices aliquet libero. Nunc id magna elit. Ut varius malesuada urna, commodo consequat ligula posuere viverra. Aliquam lobortis tellus nec ligula eleifend condimentum. Nullam volutpat nisi et sagittis consectetur. Integer fermentum a lorem quis aliquet. In laoreet sed leo et laoreet. Proin lacinia, lectus nec sagittis",
+    },
+    {
+      imagepath: wellies,
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer urna ante, semper ac enim et, ultrices aliquet libero. Nunc id magna elit. Ut varius malesuada urna, commodo consequat ligula posuere viverra. Aliquam lobortis tellus nec ligula eleifend condimentum. Nullam volutpat nisi et sagittis consectetur. Integer fermentum a lorem quis aliquet. In laoreet sed leo et laoreet. Proin lacinia, lectus nec sagittis",
+    },
+  ];
+  return pagetemplate("Peace Legal", list);
 }
